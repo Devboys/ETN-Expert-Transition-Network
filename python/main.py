f678c3c4b2c5d66e482631bb27c0c7f36f1db558
Jeppe Faber <Jeppestougaardfaber@gmail.com>
2021-11-17 14:53:18 +0100

    model name distinguishes rtn from etn

--- python/main.py
+++ python/main.py
@@ -134,7 +134,7 @@ def run(base_dir, is_param_optimizing: bool):
         power = -3 * np.random.rand()
         learning_rate = 10**(-1 + power)
 
-    model_name = f"etn_{model_id}_bs{str(batch_size)}_nb{str(n_batches)}_lr{str(learning_rate)}.pt"
+    model_name = f"rtn_{model_id}_bs{str(batch_size)}_nb{str(n_batches)}_lr{str(learning_rate)}.pt"
     model_path = model_dir + model_name
 
     train_data = ETNDataset(f"{basedir}/data/lafan1_reduced/train")
